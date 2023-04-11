6c1c72d8567f5e0d000d5b493ca28d85e2d95c56
AbhinavSinha <abhinavsinha29@gmail.com>
2023-04-11 19:17:59 +0530

    removing env variables

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-SLACK_BOT_TOKEN = 'xoxb-5083308509781-5076189038295-lhRP7k2VMA5sPU3pkVBbf2jE'
-SLACK_APP_TOKEN = 'xapp-1-A0531937FRP-5093235726116-4858d81f4812053564cfd64a9e89c62d70ee1b47bc2bd8e2d8054f3c46c0004f'
+SLACK_BOT_TOKEN = 'SLACK_BOT_TOKEN'
+SLACK_APP_TOKEN = 'SLACK_APP_TOKEN'
 
 app = App(token=SLACK_BOT_TOKEN, name='JokeBot')
 
